874492f7222dfe85ff53b58cb7ca1e7737150659
Maksuda Haider Sayma <saimahaider965@gmail.com>
2019-07-31 19:05:06 +0600

    Second Commit
    Second Commit

--- RadioCoubi/app/src/main/java/com/example/radiocoubi/Live.java
+++ RadioCoubi/app/src/main/java/com/example/radiocoubi/Live.java
@@ -1,15 +1,28 @@
 package com.example.radiocoubi;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import java.io.IOException;
 
 public class Live extends AppCompatActivity {
 
     ActionBar actionBar;
+
+    Button b_play;
+    MediaPlayer mediaPlayer;
+
+    Boolean prepared = false;
+    Boolean started = false;
+    String stream ="http://78.129.163.129:35642/;stream";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,34 @@ public class Live extends AppCompatActivity {
 
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+        b_play = (Button) findViewById(R.id.b_play);
+        b_play.setEnabled(false);
+        b_play.setText("LOADING");
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        new PlayerTask().execute(stream);
+
+
+        b_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(started){
+                    started = false;
+                    mediaPlayer.pause();
+                    b_play.setText("PLAY");
+
+                }else {
+                    started = true;
+                    mediaPlayer.start();
+                    b_play.setText("PAUSE");
+
+                }
+
+            }
+        });
     }
 
     public void onBackPressed()
@@ -38,5 +79,52 @@ public class Live extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class PlayerTask extends AsyncTask<String,Void,Boolean> {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+
+            try {
+                mediaPlayer.setDataSource(strings[0]);
+                mediaPlayer.prepare();
+                prepared = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            b_play.setEnabled(true);
+            b_play.setText("PLAY");
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(started){
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(started){
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(prepared){
+            mediaPlayer.release();
+        }
     }
 }
